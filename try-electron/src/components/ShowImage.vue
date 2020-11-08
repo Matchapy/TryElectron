3b1879793abfa11d60bc35d122f1180b4d74e85a
Ryo Omae <matcikun@gmail.com>
2020-11-09 01:18:57 +0900

    added vuetify

---
+++ try-electron/src/components/ShowImage.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <select v-model="selected">
+            <option v-for="(option, key) in options" :key="key" :value="option.text">
+                {{ option.text }}
+            </option>
+        </select>
+    </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+    name: 'ShowImage',
+    data() {
+        return {
+            selected: 'A',
+            options: [
+                { text: 'One', value: 'A'},
+                { text: 'Two', value: 'B'}
+            ]
+    }}
+})
+</script>
---
+++ try-electron/src/components/TextBox.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="textbox">
+        <textarea v-model="message" placeholder="edit here"></textarea>
+    </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+    name: 'TextBox'
+})
+</script>
+
+<style lang="sass">
+
+</style>
